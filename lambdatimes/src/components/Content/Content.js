55c7ed21e698d9d2bfe5b9d76d8a5dd28eaa00cc
James Basile <jbasile14@gmail.com>
2019-02-22 13:00:44 -0500

    had an error in the syntax of my 2nd return statement of the filterCards function, filter onClick functionality is working properly

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -51,9 +51,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards.filter(card => {
-      card.tab === this.state.selected
-    });
+    return this.state.cards.filter(card => card.tab === this.state.selected);
   };
 
   render() {
